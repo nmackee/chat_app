3d691a99d7c614d6e38b85d5feb7967d5b8962ef
nmackee <mackee.base@gmail.com>
2021-01-30 23:31:50 +0900

    機能改善

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "rooms#show"
+  get '/show_additionally', to: 'rooms#show_additionally'
 
   resources :messages, only: :create
 end
