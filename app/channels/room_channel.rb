030a6f95ece258168719e5c29c96ff4a0329cef1
nmackee <mackee.base@gmail.com>
2021-01-30 21:07:10 +0900

    チャット機能の実装

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -2,6 +2,9 @@ class RoomChannel < ApplicationCable::Channel
 
   # サーバー側からフロントを監視できているかを確認できたときに動くメソッド
   def subscribed
+    # 配信する部屋名を決定
+    stream_from "room_channel"
+
     #5.times { puts "***test***" }
   end
 
--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,18 +1,33 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("RoomChannel", {
+// turbolinksの読み込みが終わった後にidを取得しないとエラーが出る
+document.addEventListener('turbolinks:load', () => {
 
-  // フロント側からサーバー側を監視できているかを確認できたときに動く関数
-  connected() {
-    // Called when the subscription is ready for use on the server
-    // console.log('test')
-  },
+  // js.erbないで使用できるように変数を定義しておく
+  window.messageContainer = document.getElementById('message-container')
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
-
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
+  // 以下のプログラムが他のページで動作しないようにしておく
+  if (messageContainer === null) {
+    return
   }
-});
+
+  consumer.subscriptions.create("RoomChannel", {
+
+    // フロント側からサーバー側を監視できているかを確認できたときに動く関数
+    connected() {
+      // Called when the subscription is ready for use on the server
+      // console.log('test')
+    },
+
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+
+    received(data) {
+      // Called when there's incoming data on the websocket for this channel
+
+      // サーバー側から受け取ったHTMLを一番最後に加える
+      messageContainer.insertAdjacentHTML('beforeend', data['message'])
+    }
+  })
+})
